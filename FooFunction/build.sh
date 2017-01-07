728c7dbdc3e5f7aebfa97531a4df6b244d189a4b
OrKoN <alexei.rudenko@gmail.com>
2017-01-07 13:10:26 +0100

    First version

---
+++ FooFunction/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+{
+	rm FooFunction.zip
+	cd FooFunction; zip -r ../FooFunction.zip *; cd ..;
+} &> /dev/null
+
+MD5=$(md5sum -b FooFunction.zip | awk '{print $1}')
+
+{
+	aws s3 cp FooFunction.zip s3://$S3_BUCKET_NAME/FooFunction-$MD5.zip
+	aws s3 cp FooFunction.zip s3://$S3_BUCKET_NAME/FooFunction-latest.zip
+	rm FooFunction.zip
+} &> /dev/null
+
+echo $MD5
---
+++ bin/create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+(./FooFunction/build.sh)
+# TODO: add other functions here
+
+aws cloudformation create-stack --stack-name=$STACK_NAME \
+	--template-body=file://cloudformation.yaml \
+	--capabilities CAPABILITY_IAM
